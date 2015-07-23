a026ea3c5bd5ddc7675c615d979156c5e751207b
singhj <j.singh@earlystageit.com>
2015-07-23 17:43:31 +0000

    Added Diana's PNG files to the demo 	modified:   cee1/urls.py 	modified:   home/templates/base.html 	modified:   home/templates/home.html 	new file:   home/templates/narrow_network.html 	modified:   home/views.py 	new file:   nginx_default 	new file:   static/dimages/20150720_narrow_network_1.png 	new file:   static/dimages/20150720_narrow_network_2.png 	new file:   static/dimages/20150720_narrow_network_3.png 	new file:   static/dimages/20150720_z5home_1.png 	new file:   static/dimages/20150720_z5home_2.png 	new file:   static/dimages/20150720_z5home_3.png 	new file:   static/dimages/20150720_z5home_4.png

--- home/views.py
+++ home/views.py
@@ -14,9 +14,22 @@ logging.getLogger().setLevel(logging.DEBUG)
 logger = logging.getLogger('zakipoint')
 logger.setLevel(logging.DEBUG)
 
+from session import AppUser
+
 def home(request):
-    return  render_to_response('home.html', {}, context_instance=RequestContext(request) )
+    ks = sorted(request.session.keys())
+    for k in ks:
+        logger.info('%s: %s', k, request.session[k])
+    try:
+        app_user = AppUser.get_by_username(request.session['username'])
+        fullname = app_user.get_full_name()
+    except IndexError:
+        fullname = ''
+    return  render_to_response('home.html', {'session': request.session, 'fullname': fullname} )
+
+def narrow_network(request):
+    return  render_to_response('narrow_network.html', {'session': request.session} )
 
 def notyet(request):
     messages.add_message(request, messages.ERROR, 'Not implemented yet')
-    return  render_to_response('home.html', {}, context_instance=RequestContext(request) )
+    return  render_to_response('home.html', {'session': request.session} )
