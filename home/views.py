3f500933bb58442f2adab9f72b59bdf526a9b03e
singhj <j.singh@earlystageit.com>
2015-09-17 00:17:27 +0000

    Ready for a demo 	modified:   ../../home/templates/base.html 	modified:   ../../home/templates/home.html 	modified:   ../../home/templates/sign-in.html 	modified:   ../../home/views.py 	modified:   ../dimages/0911_r2.png 	new file:   ../dimages/0916_home_1.png 	modified:   ../dimages/20150820_high_risk.png 	new file:   ../dimages/cost_driver_pharmacy_Aug17.png 	new file:   ../images/favicon-transparent.png 	new file:   ../images/logo-blue-transparent.png 	modified:   ../images/logo-blue.png 	new file:   ../images/zph-logo-200sq.png

--- home/views.py
+++ home/views.py
@@ -54,7 +54,8 @@ def pop_biom(request):
         app_user = AppUser.get_by_username(request.session['username'])
         fullname = app_user.get_full_name()
     except (IndexError, KeyError):
-        return redirect('/signout/')
+        logout(request)  # logut works whether someone is signed in or not. session wiped.
+        return redirect('/')
     context = {'session': request.session, 'path': request.path, 'fullname': fullname, 'version': VERSION_STAMP}
     return  render_to_response('pop_biom.html', context)
 
@@ -63,7 +64,8 @@ def pop_risk(request):
         app_user = AppUser.get_by_username(request.session['username'])
         fullname = app_user.get_full_name()
     except (IndexError, KeyError):
-        return redirect('/signout/')
+        logout(request)  # logut works whether someone is signed in or not. session wiped.
+        return redirect('/')
     context = {'session': request.session, 'path': request.path, 'fullname': fullname, 'version': VERSION_STAMP}
     return  render_to_response('pop_risk.html', context)
 
@@ -72,7 +74,8 @@ def notyet(request):
         app_user = AppUser.get_by_username(request.session['username'])
         fullname = app_user.get_full_name()
     except (IndexError, KeyError):
-        return redirect('/signout/')
+        logout(request)  # logut works whether someone is signed in or not. session wiped.
+        return redirect('/')
     messages.add_message(request, messages.ERROR, 'Not implemented yet')
     context = {'session': request.session, 'path': request.path, 'fullname': fullname, 'version': VERSION_STAMP}
     return  render_to_response('home.html', context)
@@ -82,7 +85,8 @@ def notfound(request):
         app_user = AppUser.get_by_username(request.session['username'])
         fullname = app_user.get_full_name()
     except (IndexError, KeyError):
-        return redirect('/signout/')
+        logout(request)  # logut works whether someone is signed in or not. session wiped.
+        return redirect('/')
     context = {'session': request.session, 'path': request.path, 'fullname': fullname, 'version': VERSION_STAMP}
     return  render_to_response('404.html', context)
 
