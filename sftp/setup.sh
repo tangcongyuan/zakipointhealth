e1b7a4adceeb7409e289749f0b9a4a184a4de59e
J Singh <singhj@users.noreply.github.com>
2015-10-11 14:53:58 -0400

    setting up sftp server

---
+++ sftp/setup.sh
@@ -0,0 +1,19 @@
+sudo hostname dis
+sudo groupadd sftpAccess
+sudo groupadd adminz
+sudo yum -y update
+sudo yum -y install vsftpd
+sudo yum -y install openssh-server
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+sudo vi /etc/ssh/sshd_config
+sudo cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.orig
+sudo vi /etc/vsftpd/vsftpd.conf
+sudo systemctl restart sshd.service
+sudo systemctl restart vsftpd.service
+
+sudo useradd -m hsirx -g sftpAccess
+sudo passwd hsirx
+sudo chown root /home/hsirx
+sudo chmod 750 /home/hsirx
+sudo mkdir /home/hsirx/www
+sudo chown hsirx:sftpAccess /home/hsirx/www
