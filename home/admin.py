62927fc90de57de21554c9c809f4904d9e1f948c
singhj <j.singh@datathinks.org>
2015-11-30 11:20:14 -0500

    Added data model folder 	modified:   home/admin.py 	new file:   home/data_model/DataModel.dia 	new file:   home/data_model/DataModel.png 	new file:   home/data_model/README.md

--- home/admin.py
+++ home/admin.py
@@ -5,7 +5,7 @@ from django.contrib.admin.sites import NotRegistered
 from django.contrib.auth.models import User, Group, Permission
 from django.contrib.sites.models import Site
 
-import logging
+import logging, sys
 logger = logging.getLogger('zakipoint')
 
 from session import *
@@ -14,8 +14,16 @@ from models import *
 def unregister(cls):
     try:
         admin.site.unregister(cls)
+        #logger.info('Admin unregistered %s', cls.__name__)
     except NotRegistered:
-        pass
+        logger.info('Admin could not unregister %s', cls.__name__)
+
+def register(cls):
+    try:
+        admin.site.register(cls)
+        #logger.info('Admin registered %s', cls.__name__)
+    except NotRegistered:
+        logger.info('Admin could not register %s', cls.__name__)
 
 unregister(User)
 unregister(Group)
@@ -23,6 +31,6 @@ unregister(Company)
 #unregister(Capability)
 #unregister(UserRole)
 #admin.site.register(Company)
-admin.site.register(User)
-admin.site.register(Group)
-admin.site.register(Company)
+register(User)
+register(Group)
+register(Company)
---
+++ home/data_model/README.md
@@ -0,0 +1,6 @@
+# Data Model
+
+Built using [Dia](https://wiki.gnome.org/action/show/Apps/Dia)
+
+![](/home/data_model/DataModel.png)
+
